059be8656255e7bd57fb9c7246219d3c53cea3ad
Aldrin Vega <2018003222@dhvsu.edu.ph>
2022-12-28 16:46:16 +0800

    End of Section 5

---
+++ client-app/src/app/layout/Navbar.tsx
@@ -0,0 +1,25 @@
+import { defaultMaxListeners } from 'events';
+import react from 'react';
+import { Button, Container, Menu } from 'semantic-ui-react';
+
+interface Props{
+    openForm: () => void;
+}
+
+export default function NavBar({openForm}: Props){
+    return(
+        <Menu inverted fixed='top'>
+            <Container>
+                <Menu.Item header>
+                    <img src='/assets/logo.png' alt='logo' style={{marginRight: '10px'}}/>
+                    Activites
+                </Menu.Item>
+                <Menu.Item name = 'Activities'/>
+                <Menu.Item>
+                    <Button onClick={openForm} positive content='Create Actvity'/>
+                </Menu.Item>
+                
+            </Container>
+        </Menu>
+    )
+}
